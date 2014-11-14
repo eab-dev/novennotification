454f490c0c0a47867e01c9932d0029cef6ea509c
EAB Development Team <xian@eab.co.uk>
2014-11-14 11:19:43 +0800

    Change default language

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,10 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [RegionalSettings]
-Locale=fre-FR
-ContentObjectLocale=fre-FR
-TextTranslation=enabled
-TranslationCache=enabled
 TranslationExtensions[]=novennotification
 
 [ContentSettings]
